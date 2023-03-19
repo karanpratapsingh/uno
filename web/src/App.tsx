d6e284eb746f199853ecc72d30cf6c03e25b1b6d
Karan Pratap Singh <contact@karanpratapsingh.com>
2023-03-18 17:19:16 -0700

    fix: lint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,7 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
-  return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+  return <h1 class='text-3xl font-bold underline'>Hello world!</h1>;
 };
 
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,11 +5,4 @@ import './index.css';
 import App from './App';
 
 const root = document.getElementById('root');
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
-  );
-}
-
 render(() => <App />, root!);
