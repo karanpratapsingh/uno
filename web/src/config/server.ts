cf68ab872303a8ffc2394390c614f76f5a490ca1
Karan Pratap Singh <contact@karanpratapsingh.com>
2023-03-21 14:06:51 -0700

    feat: refactor config

--- web/src/config/server.ts
+++ web/src/config/server.ts
@@ -1,5 +1,3 @@
-export const WEB_HTTP_URL =
-  import.meta.env.VITE_WEB_HTTP_URL || 'http://localhost:3000';
 export const SERVER_HTTP_URL =
   import.meta.env.VITE_SERVER_HTTP_URL || 'http://localhost:5000';
 export const SERVER_WS_URL =
---
+++ web/src/config/web.ts
@@ -0,0 +1,2 @@
+export const WEB_HTTP_URL =
+  import.meta.env.VITE_WEB_HTTP_URL || 'http://localhost:3000';
