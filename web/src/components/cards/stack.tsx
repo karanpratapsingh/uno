5750dbe559305be0b015998022b7bbdbaf10de7a
Karan Pratap Singh <contact@karanpratapsingh.com>
2023-03-22 16:31:22 -0700

    feat: card stack

---
+++ web/src/components/cards/stack.tsx
@@ -0,0 +1,23 @@
+import { Card } from '../../types/game';
+import BlankCard from './blank';
+import UnoCard from './uno';
+
+interface CardStackProps {
+  card?: Card;
+  hidden?: boolean;
+  onClick?(): void;
+}
+
+function CardStack(props: CardStackProps): React.ReactElement {
+  const { card, hidden, onClick } = props;
+  return (
+    <div className='stack' onClick={() => onClick && onClick()}>
+      {card && <UnoCard card={card} hidden={hidden} />}
+      <BlankCard hidden={hidden} />
+      <BlankCard hidden={hidden} />
+      <BlankCard hidden={hidden} />
+    </div>
+  );
+}
+
+export default CardStack;
